198b1ef1cc1c23cb15f3cc880b93b9ca2d3fd232
Peter Lejeck <peter.lejeck@gmail.com>
2014-09-26 00:42:57 -0700

    Fix overflow bug in OpenSSL

--- src/shared.c
+++ src/shared.c
@@ -2,21 +2,24 @@
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 #include <string.h>
+#include <stdio.h>
 #include "shared.h"
 
 int base64_decode
 (char *src, bytes *dest)
 {
 	BIO *bio, *b64;
-	unsigned int max_len = strlen(src) * 0.75;
+	unsigned int max_len = strlen(src) * 3 / 4;
 
 	dest->data = malloc(max_len);
 	if (dest->data == NULL) return -1;
 
+	FILE* stream = fmemopen(src, strlen(src), "r");
 	b64 = BIO_new(BIO_f_base64());
-	bio = BIO_new_mem_buf(src, -1);
+	bio = BIO_new_fp(stream, BIO_NOCLOSE);
 	bio = BIO_push(b64, bio);
-	dest->length = (int)BIO_read(bio, dest->data, max_len);
+	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
+	dest->length = BIO_read(bio, dest->data, max_len);
 	BIO_free_all(bio);
 
 	return 0;
